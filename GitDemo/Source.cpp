ab6f55dd0e9e7cf15022a5aaf453c681b75613b5
Jingtian Li <406159333@qq.com>
2021-01-23 13:14:20 -0600

    added another remote repo, added notes for pushing to new remote repo on git

--- GitDemo/Source.cpp
+++ GitDemo/Source.cpp
@@ -33,3 +33,20 @@
 
 //show changes
 //git diff
+
+//show detailed log
+//git log
+
+//show oneline log
+//git reflog
+
+//push local repo to remote empty new repo, here "origin" is an arbitray name, you can change it to whatever, you can add multiple ones if you want
+	/*
+	1, add remote repo as your remote origin master: (the pass word can be your PAT)
+		git remote add origin http://username:password@github.com/...
+	2, push (this also redirect the connected remote repo if you change the name origin to the name of another remote repo)
+		git push --set-upstream origin main
+	*/
+
+//remove connection to remote repo nameoftheremote should be origin if not complicated
+//git remote remove nameoftheremote
