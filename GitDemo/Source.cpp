046dacf01a842bde26e2a5308d38a302aaef82b0
Jingtian Li <406159333@qq.com>
2021-01-23 12:26:00 -0600

    Added basic creation and clone notes

--- GitDemo/Source.cpp
+++ GitDemo/Source.cpp
@@ -1,1 +1,29 @@
-//
+//initialize a git repo
+//git init
+
+//to add .gitignore
+//copy con .gitignore, then press ctrl + z and Enter.
+
+//to stage a file 
+//git add filename
+
+//to stage a folder
+//git add foldername/
+
+//to stage all 
+//git add -A
+
+//to unstage a file
+//git reset filename
+
+//to unstage all
+//git reset 
+
+//to commit 
+//git commit -m "commit message"
+
+//clone remote repo
+//git clone https://github.com/glfw/glfw.git
+
+//clone remote repo right at the folder, do not create subfolder:
+//git clone https://github.com/glfw/glfw.git .
