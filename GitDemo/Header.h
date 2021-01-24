a98193dcceee665fc1f123b4bf13f5e87174c5bb
Jingtian Li <406159333@qq.com>
2021-01-23 20:42:03 -0600

    a lot of changes to header.h

--- GitDemo/Header.h
+++ GitDemo/Header.h
@@ -1,16 +1,7 @@
 #pragma once
 Former first lady Melania Trump is officially off - duty, 
 a message she made loudand clear while exiting the 
-plane with Donald Trump upon their arrival in Florida 
-after leaving D.C.In a video clip that quickly went viral, 
-Melaniaand Donald are greeted by a crowd of photographers
-awaiting them;and while Donald stops for a photo,
-Melania abruptly walks off in the other direction.Naturally, 
-Twitter is having a field day with the footage, only one 
-of many clips that suggest Melania has not been enjoying 
-her role as a public figure, mother fuck beside Donald, for 
-some time now.With Donald’s presidential term at an end, 
-Melania is no longer dealing with the same level of public
-pressure as she felt in the White House, and many are eager
-to see whether her true feelings about her husband will 
-become clearer in this next chapter.
+plane with Donafld TrfsdfsfIn a video ssdclip that quickly went viral, 
+Melaniaansf Dofdsald are greeted by a crowd of photographers
+awaitingfsdabrusptlsy walks off indffsdesidential term at an end, 
+Melania is no ser true st dchapfster.
